696ae6dd01ea17ebd339fab5b530adf2d28c5fe7
bhaswargupta1 <80271642+bhaswargupta1@users.noreply.github.com>
2022-01-17 16:47:26 +0530

    Add files via upload

---
+++ assignmet-2/index.js
@@ -0,0 +1,21 @@
+﻿exports.handler = async (event) => { 
+let randomvalues = []; 
+if (event.queryStringParameters && event.queryStringParameters['n']) {
+let n = parseInt(event.queryStringParameters['n']); 
+for (let i=0; i<n; i++){ 
+randomvalues.push(Math.floor(Math.random() * 10)); 
+
+}
+}
+let responseBody = randomvalues;
+const response = { 
+    statusCode: 200, 
+    headers: {
+    "Access- Control-Allow-Headers" : "Content-Type", 
+    "Access-Control-Allow-Origin" : "*",
+    "Access- Control-Allow-Methods" : "OPTIONS, POST ,GET" 
+    }, 
+    body: JSON. stringify(responseBody),
+    }; 
+    return response; 
+}
